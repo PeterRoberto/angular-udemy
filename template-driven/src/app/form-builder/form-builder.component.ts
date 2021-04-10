fbe9a6b62da21f00963821a1a110526b3188f37d
Peter Roberto - Github <peterrobertoweb@gmail.com>
2021-04-10 11:25:14 -0300

    Avançando no form array, pegar  infos dos campos de um formulário, usando array, replicando campos etc..

--- template-driven/src/app/form-builder/form-builder.component.ts
+++ template-driven/src/app/form-builder/form-builder.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form-builder',
@@ -8,19 +8,34 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class FormBuilderComponent implements OnInit {
 
-  clientForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    address: new FormGroup({
-      street: new FormControl(''),
-      city : new FormControl(''),
-      state: new FormControl(''),
+  // clientForm = new FormGroup({
+  //   firstName: new FormControl(''),
+  //   lastName: new FormControl(''),
+  //   address: new FormGroup({
+  //     street: new FormControl(''),
+  //     city : new FormControl(''),
+  //     state: new FormControl(''),
+  //   })
+  // });
+
+  clientForm = this.fb.group({
+    firstName: [''],
+    lastName: [''],
+    address: this.fb.group({
+      street: [''],
+      city : [''],
+      state: [''],
     })
   });
 
-  constructor() { }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  submit() {
+    console.log(this.clientForm.value);
   }
 
 }
